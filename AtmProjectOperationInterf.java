9077422fb72fd533917ef5ab0601e6e60d688407
MANIGANDLA SUPRIYA <supriya180702@gmail.com>
2024-12-10 18:38:07 +0530

    this is my first commit

---
+++ AtmProjectOperationInterf.java
@@ -0,0 +1,7 @@
+package AtmProject;
+public interface AtmProjectOperationInterf {
+	public void viewBalance();
+	public void withdrawAmount(double withdrawAmount);
+	public void depositAmount(double depositAmount);
+	public void viewMiniStatement();
+}
